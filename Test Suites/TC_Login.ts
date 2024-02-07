26e9efa42f48e6379835c03bb6ffa5e91fc65fa8
chyntiaclaudia <chyntiaclaudia13@gmail.com>
2024-02-07 10:35:29 +0700

    test

--- Test Suites/TC_Login.ts
+++ Test Suites/TC_Login.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>e7dc4669-0625-41a0-bca7-93f4932b5824</testSuiteGuid>
+   <testCaseLink>
+      <guid>83425a59-9fd6-455f-9f63-c20c9f404aed</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
